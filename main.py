6ea01f13407163c3d70955a0924195a3d207ca66
Alex-Tibulschi <gabakejsi@yahoo.com>
2025-09-22 21:44:04 +0100

    #3 Added procedurally generated map

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 def main() -> None:
 
@@ -14,6 +14,11 @@ def main() -> None:
     #Size of game map
     map_width = 80
     map_height = 45
+
+    #Range of size of room and max rooms in a level
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     #Tileset for icons, player, npc etc.
     tileset = tcod.tileset.load_tilesheet(
@@ -29,7 +34,14 @@ def main() -> None:
     entities = {npc, player}
 
     #Initialise map
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     #Initialise Engine
     engine = Engine(entities= entities, event_handler= event_handler, game_map= game_map, player= player)
